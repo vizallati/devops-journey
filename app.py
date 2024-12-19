cd913c276ff6e3310fb6214850aec5b438899c17
Ian Ekwoge <iekwoge@yahoo.com>
2024-12-19 10:13:00 +0000

    Adds timeline entry endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from data import get_timeline_data
+from flask import Flask, render_template, request
+from data import get_timeline_data, add_timeline_entry
+from loguru import logger
 
 app = Flask(__name__)
 
@@ -7,6 +8,16 @@ app = Flask(__name__)
 def timeline():
     timeline_data = get_timeline_data()
     return render_template('devops-timeline.html', timeline_data=timeline_data)
+
+@app.route('/add-timeline-entry', methods=['POST'])
+def add_entry():
+    entry_data = request.json
+    logger.info(entry_data)
+    request_data = []
+    for k,v in entry_data.items():
+        request_data.append(v)
+    add_timeline_entry(request_data)
+    return "Timeline entry successfully added"
 
 if __name__ == '__main__':
     app.run(debug=True)
