a07041db317955df4fd52f9c4266593bb7b1ba53
Ian Ekwoge <iekwoge@yahoo.com>
2025-02-08 03:33:47 +0000

    Point to absolute path for uploads dir

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = os.urandom(24)
-UPLOAD_FOLDER = 'static/images'
+UPLOAD_FOLDER = '/app/static/images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
