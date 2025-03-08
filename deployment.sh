0e1e49d913f83f733b24ca9f25c09dc65216c0df
Ian Ekwoge <iekwoge@yahoo.com>
2025-03-08 07:08:35 +0000

    No downtime deployment

---
+++ deployment.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+
+echo "Starting backup server..."
+sudo docker-compose up -d web_app_backup
+# todo: add checks for status of container
+echo "Switching to backup server"
+sudo sed -i s/5000/5001/g /etc/nginx/sites-available
+sudo nginx -s reload
+
+echo "Rebuilding main server"
+sudo docker-compose up -d --build --force-recreate web_app
+sleep 10
+
+echo "Switching to main server"
+sudo sed -i s/5001/5000/g /etc/nginx/sites-available
+sudo nginx -s reload
+
+
+echo "Stopping backup server"
+sudo docker-compose stop web_app_backup
+sudo docker-compose rm web_app_backup
+
+
