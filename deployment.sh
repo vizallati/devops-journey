9b430f530dfe3556d6ba6013d22dce76eebcb97f
Ian Ekwoge <iekwoge@yahoo.com>
2025-03-08 10:06:43 +0000

    modify deployment script

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,7 @@
 
 
 echo "Starting backup server..."
-sudo docker-compose up -d web_app_backup
+sudo docker-compose up -d --build web_app_backup
 # todo: add checks for status of container
 echo "Switching to backup server"
 sudo sed -i s/5000/5001/g /etc/nginx/sites-available/default
@@ -11,6 +11,8 @@ sudo nginx -s reload
 echo "Stopping main server"
 sudo docker-compose stop web_app
 sudo docker-compose rm -f web_app
+container_name=$(sudo docker images | grep web_app | cut -d ' ' -f 1)
+sudo docker rmi -f "$container_name"
 
 echo "Rebuilding main server"
 sudo docker-compose up -d --no-deps --build web_app
@@ -24,5 +26,7 @@ sudo nginx -s reload
 echo "Stopping backup server"
 sudo docker-compose stop web_app_backup
 sudo docker-compose rm -f web_app_backup
+container_name=$(sudo docker images | grep web_app_backup | cut -d ' ' -f 1)
+sudo docker rmi -f "$container_name"
 
 
