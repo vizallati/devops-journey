1627c222299d7b62899843a6ca63c038da686508
Ian Ekwoge <iekwoge@yahoo.com>
2025-03-21 08:45:29 +0000

    Initial implementation articles page

--- app_utils.py
+++ app_utils.py
@@ -1,8 +1,18 @@
-from flask import session, redirect, url_for
+from flask import session
+import requests
 
+
+json_converter_endpoint = "https://api.rss2json.com/v1/api.json"
+medium_username = "iekwoge"
 
 def check_user_auth():
     try:
         return session['loggedin']
     except KeyError:
         return False
+
+
+
+def convert_rrs_to_json():
+     response = requests.get(f'{json_converter_endpoint}?rss_url=https:%2F%2Fmedium.com%2F@{medium_username}5%2Ffeed')
+     return response.json()["items"]
